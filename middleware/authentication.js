bc2976ca085ece6cc199ce611bf7a3164ed62ee5
Surajkumar0708 <srjthakur9@gmail.com>
2025-04-16 20:23:03 +0530

    passed the error msg

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,12 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const authenticateToken = (req, res, next) => {
   const token = req?.headers?.authorization?.split(" ")[1];
+  console.log("============= test", token);
   if (!token) {
     return res.sendStatus(401);
   }
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      console.log("========== err", err);
+      return res.sendStatus(403).json({ error: err });
     } else {
       req.user = user;
       next();
