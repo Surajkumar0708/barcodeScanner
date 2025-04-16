a4bc22945b37687720588144b4396878f2abfdf1
Surajkumar0708 <srjthakur9@gmail.com>
2025-04-16 23:02:31 +0530

    correct the addorder and getorders api

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,9 +3,9 @@ import Order from "../models/Order.js";
 export const addOrder = async (req, res) => {
   try {
     const order = {
-      userId: req?.user?.userId,
-      items: req?.user?.items,
-      total: req?.user?.total,
+      userId: req?.body?.userId,
+      items: req?.body?.items,
+      total: req?.body?.total,
     };
     const updatedOrder = await Order.insertOne(order);
     return res
